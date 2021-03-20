cadcb51c46edc2c0cb840ba5df4c5561e5fde1a3
loajanc <kshyun1432@gmail.com>
2021-03-20 11:16:39 +0900

    For TestCase

--- basic/start.js
+++ basic/start.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+// NODE JS로 테스트 케이스 받기
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("line", function(line) {
+  console.log("hello !", line);
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
